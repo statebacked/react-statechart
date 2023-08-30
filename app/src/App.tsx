cb450a4764c00d1c65580b4abf5c616963ec379f
Adam Berger <adam@simplystated.dev>
2023-08-30 17:40:42 -0400

    feat: editing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -9,80 +9,82 @@ function App() {
   return (
     <>
       <h1>Hi</h1>
-      <GridBackground>
-        <FlowGraph
-          editable={{
-            getAvailableStates() {
-              return [];
-            },
-            onAddTransition(sourceState) {},
-            onDeleteStateItem(stateId, itemId) {},
-            onDeleteTransition(sourceState, targetState, event, condition) {},
-            onDeleteTransitionItem(
-              sourceState,
-              targetState,
-              event,
-              condition,
-              itemId
-            ) {},
-            onRemoveState(stateId) {},
-            onUpdateState(stateId, state) {},
-            onUpdateTransition(
-              sourceState,
-              targetState,
-              event,
-              condition,
-              updated
-            ) {},
-            onUpdateTransitionTarget(previousTargetId, newTargetId) {},
-            onUpsertStateItem(stateId, item) {},
-            onUpsertTransitionItem(
-              sourceState,
-              targetState,
-              event,
-              condition,
-              item
-            ) {},
-          }}
-          flow={{
-            name: "my flow",
-            assertions: [],
-            entryActions: [],
-            exitActions: [],
-            id: "" as any,
-            metadata: {
-              actions: {},
-              assertions: {},
-              conditions: {},
-              events: {},
-            },
-            requirements: [],
-            states: {
-              ["state1" as schema.StateId]: {
-                assertions: [],
-                collateral: [],
-                entryActions: [],
-                exitActions: [],
-                name: "state1",
-                transitions: [],
-                type: "compound",
+      <div style={{ overflow: "hidden", width: "100%", height: 500 }}>
+        <GridBackground as="section">
+          <FlowGraph
+            editable={{
+              getAvailableStates() {
+                return [];
               },
-              ["state2" as schema.StateId]: {
-                assertions: [],
-                collateral: [],
-                entryActions: [],
-                exitActions: [],
-                name: "state2",
-                transitions: [],
-                type: "atomic",
-                parent: "state1" as schema.StateId,
+              onAddTransition(_sourceState) {},
+              onDeleteStateItem(_stateId, _itemId) {},
+              onDeleteTransition(_sourceState, _targetState, _event, _condition) {},
+              onDeleteTransitionItem(
+                _sourceState,
+                _targetState,
+                _event,
+                _condition,
+                _itemId,
+              ) {},
+              onRemoveState(_stateId) {},
+              onUpdateState(_stateId, _state) {},
+              onUpdateTransition(
+                _sourceState,
+                _targetState,
+                _event,
+                _condition,
+                _updated,
+              ) {},
+              onUpdateTransitionTarget(_previousTargetId, _newTargetId) {},
+              onUpsertStateItem(_stateId, _item) {},
+              onUpsertTransitionItem(
+                _sourceState,
+                _targetState,
+                _event,
+                _condition,
+                _item,
+              ) {},
+            }}
+            flow={{
+              name: "my flow",
+              assertions: [],
+              entryActions: [],
+              exitActions: [],
+              id: "" as any,
+              metadata: {
+                actions: {},
+                assertions: {},
+                conditions: {},
+                events: {},
               },
-            },
-            transitions: [],
-          }}
-          selectedItems={[]}
-        />
-      </GridBackground>
+              requirements: [],
+              states: {
+                ["state1" as schema.StateId]: {
+                  assertions: [],
+                  collateral: [],
+                  entryActions: [],
+                  exitActions: [],
+                  name: "state1",
+                  transitions: [],
+                  type: "compound",
+                },
+                ["state2" as schema.StateId]: {
+                  assertions: [],
+                  collateral: [],
+                  entryActions: [],
+                  exitActions: [],
+                  name: "state2",
+                  transitions: [],
+                  type: "atomic",
+                  parent: "state1" as schema.StateId,
+                },
+              },
+              transitions: [],
+            }}
+            selectedItems={[]}
+          />
+        </GridBackground>
+      </div>
     </>
   );
 }
