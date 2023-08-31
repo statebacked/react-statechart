79077c55a641f5f10f1839ad981d8cfc3b5d4dda
Adam Berger <adam@simplystated.dev>
2023-08-31 10:16:42 -0400

    feat: accept style prop

--- src/components/grid-background/index.tsx
+++ src/components/grid-background/index.tsx
@@ -1,18 +1,20 @@
-import { ElementType, ReactNode } from "react";
+import { CSSProperties, ElementType, ReactNode } from "react";
 import styles from "./grid-background.module.css";
 
 export const GridBackground = ({
   as,
   className,
   children,
+  style,
 }: {
   as?: ElementType;
   className?: string;
+  style?: CSSProperties;
   children: ReactNode;
 }) => {
   const El = as ?? "div";
   return (
-    <El className={`${styles.gridBackground} ${className ?? ""}`}>
+    <El className={`${styles.gridBackground} ${className ?? ""}`} style={style}>
       {children}
     </El>
   );
