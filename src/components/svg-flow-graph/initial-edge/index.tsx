025727cf5446233e69de9726e4779f803ed2f442
Adam Berger <adam@simplystated.dev>
2023-09-13 17:18:54 -0400

    feat: better initial state for svg

--- src/components/svg-flow-graph/initial-edge/index.tsx
+++ src/components/svg-flow-graph/initial-edge/index.tsx
@@ -3,13 +3,13 @@ import { ArrowMarker } from "../arrow-marker";
 
 export const InitialEdge = ({ pos }: { pos: PositionInfo }) => {
   const endPoint = {
-    x: pos.x - 10,
-    y: pos.y + 10,
+    x: pos.x - 7,
+    y: pos.y + 7,
   };
 
   const startPoint = {
-    x: endPoint.x - 5,
-    y: endPoint.y - 10,
+    x: endPoint.x - 3.5,
+    y: endPoint.y - 7,
   };
 
   const markerId = `n${Math.floor(Math.random() * 1000)}`;
@@ -19,7 +19,7 @@ export const InitialEdge = ({ pos }: { pos: PositionInfo }) => {
       <defs>
         <ArrowMarker id={markerId} />
       </defs>
-      <circle r="4" cx={startPoint.x} cy={startPoint.y} fill="#000" />
+      <circle r="3" cx={startPoint.x} cy={startPoint.y} fill="#000" />
       <path
         d={`M ${startPoint.x},${startPoint.y} Q ${startPoint.x},${endPoint.y} ${
           endPoint.x
